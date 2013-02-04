7d62cd14beaadcf44b06320b40540dc61bf5cf97
Sebastian Laskawiec <sebastian.laskawiec@payu.pl>
2013-02-04 15:45:27 +0100

    Added missing Javadocs
    fix #1

--- source/ocm/src/main/java/com/github/ocm/annotations/CSVField.java
+++ source/ocm/src/main/java/com/github/ocm/annotations/CSVField.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.ocm.annotations;
 
 import java.lang.annotation.ElementType;
@@ -10,17 +5,43 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.github.ocm.converters.DefaultDoubleConverter;
+import com.github.ocm.converters.DefaultFloatConverter;
 import com.github.ocm.converters.ICSVTypeConverter;
 import com.github.ocm.converters.MarkerConverter;
+import com.github.ocm.mapping.OCMEngine;
 
+/**
+ * CSV Field annotation. Each field is represented as column in CSV file. If
+ * field needs a convert, one must provide it as {@link #converter()} field.
+ *
+ * @author Sebastian Laskawiec
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface CSVField {
 
+    /**
+     * Converter used for text to target object convention. There are several
+     * building converters, for example: {@link DefaultFloatConverter} or
+     * {@link DefaultDoubleConverter}. There is also one converter with special
+     * type - {@link MarkerConverter}. {@link OCMEngine} ignores it during
+     * processing and tries to figure proper converter up.
+     *
+     * @return Converter for this type of field.
+     */
     public Class<? extends ICSVTypeConverter<?>> converter() default MarkerConverter.class;
 
+    /**
+     * CSV column representation.
+     *
+     * @return Representation of CSV column.
+     */
     public String csvFieldName() default "";
 
+    /**
+     * @return <code>true</code> if this is optional field.
+     */
     public boolean nullable() default true;
 
 }
--- source/ocm/src/main/java/com/github/ocm/annotations/CSVFieldIgnore.java
+++ source/ocm/src/main/java/com/github/ocm/annotations/CSVFieldIgnore.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.github.ocm.mapping.OCMEngine;
+
+/**
+ * CSV ignore annotation. This annotation works very similar to
+ * <code>transient</code> fields. {@link OCMEngine} will ignore fields annotated
+ * with this annotation.
+ *
+ * @author Sebastian Laskawiec
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface CSVFieldIgnore {
